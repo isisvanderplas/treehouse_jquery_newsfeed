6771f8bf60adbde690d2125b2df9c8cbab397809
Isis van der Plas <isisvanderplas@hotmail.com>
2018-01-10 23:35:37 +0100

    create unobtrusive checkbox

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
+const $pdfCheckbox = $("<label><input type='checkbox'> Allow PDF downloads</label>")
+
+$('#links').append($pdfCheckbox);
 
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
 
 $secureLinks.addClass('secure');
 $pdfs.addClass('pdf');
+
+$pdfs.on('click', function(event) {
+    // check if checkbox is checked
+    // if zero checkboxes is checked
+      // prevent download of document
+      // alert the user
+    if ($(':checked').length === 0) {
+      event.preventDefault();
+      alert('please check the box to allow pdf-downloads.');
+    }
+  });
